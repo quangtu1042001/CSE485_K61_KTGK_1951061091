5191e98fda3c68e7de90bf70611518d35a234c10
satthudatinh <quangtu104@gmail.com>
2021-10-21 14:11:55 +0700

    Update config.php

--- src/config.php
+++ src/config.php
@@ -2,7 +2,7 @@
         define('HOST','localhost');
         define('USER','root');
         const PASS  = '';
-        const DB    = 'ktgk'; 
+        const DB    = 'db_ktgk'; 
         $conn = mysqli_connect(HOST,USER, PASS,DB);
         if(!$conn){
             die('Không thể kết nối');
